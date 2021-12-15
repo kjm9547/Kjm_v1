998fbbd86e40dbc7343142612515f4737e220b64
kjm9547 <woals8115@naver.com>
2021-12-15 22:57:27 +0900

    d4 press enter submit end value

--- components/Addtodo.js
+++ components/Addtodo.js
@@ -1,17 +1,28 @@
 import React, {useState} from "react";
-import {View,Text,StyleSheet, TextInput,Image} from 'react-native';
+import {View,Text,StyleSheet, TextInput,Image, TouchableOpacity, Keyboard} from 'react-native';
 
 function Addtodo(){
     
     const [text,setText] = useState('');
     console.log(text);
+    const onpress =()=>{
+        setText('');
+        Keyboard.dismiss();
+    }
     return(
 <View style={styles.block}>
-        <TextInput placeholder="할일을 입력하세요" style={styles.imput}
-        value={text} onChangeText={setText}></TextInput>
+        <TextInput placeholder="할일을 입력하세요"
+         style={styles.imput}
+         value={text}
+         onChangeText={setText}
+         onSubmitEditing ={onpress}
+         returnKeyType="done">
+        </TextInput>
+        <TouchableOpacity activeOpacity={0.5}>
         <View>
-            <Image source={require('../assets/images/young_and_happy.png')} style={styles.bt_st}></Image>
+            <Image source={require('../assets/images/add_white.png')} style={styles.bt_st}></Image>
         </View>
+        </TouchableOpacity>
 </View>
     )
 }
